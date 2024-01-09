41ef698b53f11502e17ab1d37b1e3651c470146a
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-09 16:31:16 +0545

    feat: working api call in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '@/hooks/reduxHooks'
+import { fetchProducts, ProductsSelector } from '@/features/products/productsSlice'
 import NavBar from "@/components/misc/NavBar"
 import Home from "@/pages/Home"
 
 const App = () => {
+  const products = useAppSelector(ProductsSelector)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    if (products.products.length === 0) {
+      dispatch(fetchProducts())
+    }
+  }, [dispatch, products.products.length])
+
+  useEffect(() => {
+    console.log(products)
+  }, [products])
+
   return (
     <div className='min-h-[100dvh] bg-gray-800 text-white'>
       <NavBar />
