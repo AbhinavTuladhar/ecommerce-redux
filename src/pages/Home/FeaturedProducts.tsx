1ff5c2be99002d6fd9d185373f75285bd248b4b7
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-10 12:01:31 +0545

    style: make hero, featured products responsive

--- src/pages/Home/FeaturedProducts.tsx
+++ src/pages/Home/FeaturedProducts.tsx
@@ -4,18 +4,18 @@ import Furniture from '@/assets/furniture-cropped.jpg'
 import React from 'react'
 
 const FeatureCardFirst = () => (
-  <article className='flex flex-col justify-center items-center h-full px-4 py-12 text-black gap-y-7 bg-[#c8a938] rounded-xl'>
+  <article className='flex flex-col justify-center items-center h-full px-4 py-4 lg:py-12 text-black gap-y-7 bg-[#c8a938] rounded-xl'>
     <div className='flex flex-col items-center gap-y-1'>
       <h2 className='text-2xl font-semibold uppercase'> Newest clothes </h2>
       <h3 className='text-5xl font-semibold uppercase'> Big sale </h3>
     </div>
-    <img src={Clothes} className='max-h-80' alt='some clothes' />
+    <img src={Clothes} className='w-full max-h-80 max-w-80' alt='some clothes' />
   </article>
 )
 
 const FeatureCardSecond = () => (
-  <article className='flex flex-row justify-between items-center h-full px-4 text-white bg-[#fb8787] rounded-xl'>
-    <img src={Electronics} className='max-h-80' alt='some electronics' />
+  <article className='flex flex-col py-4 lg:py-0 lg:flex-row justify-between items-center h-full px-4 text-white bg-[#fb8787] rounded-xl'>
+    <img src={Electronics} className='w-full h-auto max-w-80' alt='some electronics' />
     <div className='flex flex-col justify-center gap-y-7'>
       <div className='flex flex-col gap-y-2'>
         <h2 className='text-2xl uppercase'> Latest tech </h2>
@@ -29,7 +29,7 @@ const FeatureCardSecond = () => (
 )
 
 const FeatureCardThird = () => (
-  <article className='flex flex-row justify-between items-center h-full px-4 bg-[#ff9564] rounded-xl'>
+  <article className='flex flex-col py-4 lg:py-0 lg:flex-row justify-between items-center h-full px-4 bg-[#ff9564] rounded-xl'>
     <div className='flex flex-col items-center gap-1'>
       <div className='grid w-48 h-24 font-semibold text-black'>
         <span className='flex items-center justify-center h-full col-start-1 row-start-1 text-4xl font-bold'> 20% </span>
@@ -44,7 +44,7 @@ const FeatureCardThird = () => (
       </div>
       <h3 className='text-sm uppercase'> Enjoy your new sofa </h3>
     </div>
-    <img src={Furniture} className='max-h-60 max-w-96' alt='a bunch of furniture' />
+    <img src={Furniture} className='w-full max-h-60 max-w-96' alt='a bunch of furniture' />
   </article>
 )
 
@@ -52,8 +52,8 @@ const FeaturedProducts: React.FC = () => {
   return (
     <div className='flex flex-col gap-8 py-8 mx-auto w-container'>
       <h2 className='text-3xl font-semibold uppercase'> Featured products </h2>
-      <div className='grid grid-flow-col grid-cols-2 grid-rows-2 auto-rows-fr gap-9'>
-        <div className='row-span-2'>
+      <div className='flex flex-col lg:grid lg:grid-flow-col lg:grid-cols-2 lg:grid-rows-2 gap-9'>
+        <div className='row-span-1 lg:row-span-2'>
           <FeatureCardFirst />
         </div>
         <div className='row-span-1'>
--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -2,20 +2,19 @@ import HeroImage from '@/assets/ecommerce marketing.jpg'
 
 const Hero = () => {
   return (
-    <section className='flex items-center bg-main-green min-h-96 text-white'>
-      <div className='grid content-center min-h-full py-4 mx-auto place-content-center place-items-center grid-cols-hero gap-x-32 w-container'>
-        <div className='flex flex-col justify-center h-full gap-y-8'>
-          <h1 className='text-6xl font-bold tracking-wider uppercase'>
+    <section className='flex items-center text-white bg-main-green min-h-96'>
+      <div className='grid content-center min-h-full grid-cols-1 py-4 mx-auto place-content-center place-items-center md:grid-cols-hero gap-x-20 gap-y-10 w-container'>
+        <div className='flex flex-col items-center justify-center h-full text-center md:text-left gap-y-8'>
+          <h1 className='text-6xl font-bold tracking-wider text-center uppercase md:text-left'>
             Elevate Your Lifestyle
           </h1>
           <p>
             Discover fashion trends, unleash tech marvels, step into comfort with footwear, and elevate your space with chic furniture.
           </p>
-          <button className='self-start px-4 py-3 text-2xl font-semibold uppercase bg-white rounded-md text-main-green'>
+          <button className='self-center px-4 py-3 text-2xl font-semibold uppercase bg-white rounded-md md:self-start text-main-green'>
             Learn more
           </button>
         </div>
-        {/* <p> Test </p> */}
         <img src={HeroImage} alt='computers' />
       </div>
     </section>
