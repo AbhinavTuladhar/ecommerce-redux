2c17e831e6879cb40412bee3bacfb02763138d48
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-12 15:02:03 +0545

    refactor: button in NoItemInfo

--- src/pages/Cart/NoItemInfo.tsx
+++ src/pages/Cart/NoItemInfo.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom'
+import { FaArrowLeft } from 'react-icons/fa'
 
 const NoItemInfo = () => (
   <section className="my-4 flex flex-1 flex-col items-center justify-center gap-y-4 bg-lightmode-green px-4 py-20 text-center text-white dark:bg-darkmode-green">
@@ -7,8 +8,10 @@ const NoItemInfo = () => (
       <span className="whitespace-nowrap"> {`>:-(`}</span>
     </h2>
     <NavLink to="/" className="fluid-text-xl">
-      {' '}
-      Shop now!{' '}
+      <button className="flex flex-row items-center gap-x-4 rounded-lg bg-slate-600 p-4 duration-500 hover:bg-slate-500 dark:bg-slate-900 hover:dark:bg-slate-800">
+        <FaArrowLeft />
+        <span> Continue shopping </span>
+      </button>
     </NavLink>
   </section>
 )
