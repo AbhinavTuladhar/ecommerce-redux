d82d1a97197db59624a5a641314d35466049b5fc
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-16 10:42:26 +0545

    define rest of the types for checkout form

--- src/pages/Checkout/formsTypes.d.ts
+++ src/pages/Checkout/formsTypes.d.ts
@@ -9,6 +9,46 @@ export interface BillingInfoType {
 
 export type BillingInfoNames = keyof BillingInfoType
 
+// One-liner for billing method.
+export type BillingMethod = 'fedex' | 'dhl'
+
+/**
+ * Payment method start
+ */
+interface CreditCard {
+  cardNumber: string,
+  cardHolder: string,
+  expirationDate: Date,
+  CVC: number
+}
+
+interface Paypal {
+  firstName: string,
+  lastName: string,
+  email: string
+}
+
+
+interface BillingCreditCard {
+  creditCard: CreditCard,
+  paypal?: never
+}
+
+interface BillingPayPal {
+  creditCard?: never,
+  paypal: Paypal
+}
+
+// Either CreditCard OR Paypal - not both.
+export type BillingMethod = BillingCreditCard | BillingPayPal
+/**
+ * Payment method end
+ */
+
+export type AdditionalInformation = string | undefined
+
+export type ConfirmationFlags = [boolean, boolean]
+
 export type ChildSectionName = 'billingInfo'
 
 export interface MasterFormType {
