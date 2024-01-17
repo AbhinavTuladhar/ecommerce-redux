64f18e1761d7ffaa0135b81a0786b3f1bae8ce97
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-17 21:31:42 +0545

    feat: add products page

--- src/pages/ProductsList/index.tsx
+++ src/pages/ProductsList/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
 import useProducts from '@/hooks/useProducts'
 import ProductGrid from '@/components/misc/ProductGrid'
+import PageLayout from '@/components/layouts/PageLayout'
+import PageTitle from '@/components/misc/PageTitle'
 
 const Index = () => {
   const productsResponse = useProducts()
 
-  return <ProductGrid productsState={productsResponse} />
+  return (
+    <PageLayout>
+      <PageTitle> Our Products </PageTitle>
+      <main className="py-10">
+        <ProductGrid productsState={productsResponse} />
+      </main>
+    </PageLayout>
+  )
 }
 
 export default Index
