89436d372b13d3fdf2ddcda09ce977ac969eb926
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-17 21:31:21 +0545

    feat: add categoriesAPI layer

---
+++ src/services/categoriesAPI.ts
@@ -0,0 +1,20 @@
+import BASE_URL from "./base";
+
+export const CategoriesAPI = {
+  getCategories: async () => {
+    try {
+      const url = `${BASE_URL}/products/categories`
+      const response = await fetch(url)
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} - ${response.statusText}`)
+      }
+
+      const data: string[] = await response.json()
+      return data
+    } catch (error) {
+      console.error("Error when getting category information.")
+      throw error
+    }
+  }
+}
