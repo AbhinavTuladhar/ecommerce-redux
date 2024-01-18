0f478ca7fc6f7a9599ce71cb2317e04063b45391
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-18 14:21:08 +0545

    feat: add filtering in product list

--- src/pages/ProductsList/index.tsx
+++ src/pages/ProductsList/index.tsx
@@ -1,24 +1,36 @@
+import { useState, useEffect } from 'react'
 import useProducts from '@/hooks/useProducts'
 import ProductGrid from '@/components/misc/ProductGrid'
 import PageLayout from '@/components/layouts/PageLayout'
 import PageTitle from '@/components/misc/PageTitle'
 import useCategories from '@/hooks/useCategories'
 import convertToTitleCase from '@/helpers/convertToTitleCase'
-import { useState } from 'react'
+import type { ProductsType } from '@/features/products/productsSlice'
 
 const Index = () => {
   const productsResponse = useProducts()
+  const { products: allProducts } = productsResponse
   const { data, error, loading } = useCategories()
+
+  const [currentProducts, setCurrentProducts] = useState<ProductsType[]>([])
+
+  // Keep track of the selected tab.
+  const [activeTab, setActiveTab] = useState<string>('All')
 
   // Append an 'all' option to the categories array
   const categoryList = ['All', ...data]
-
-  const [activeTab, setActiveTab] = useState<string>('All')
 
   const handleClick = (categoryName: string) => {
     setActiveTab(categoryName)
   }
 
+  useEffect(() => {
+    setCurrentProducts(() =>
+      activeTab === 'All' ? allProducts : allProducts.filter((product) => product.category === activeTab),
+    )
+  }, [activeTab, allProducts])
+
+  // If active - blue text and border. If not, normal colours according to theme.
   const tabs = categoryList.map((category, index) => (
     <li
       className={`flex w-36 justify-center whitespace-nowrap border-b-2 bg-gray-100 p-4 text-black duration-500 hover:cursor-pointer hover:brightness-90 dark:bg-slate-950  dark:hover:brightness-110 lg:w-44 ${
@@ -33,6 +45,11 @@ const Index = () => {
     </li>
   ))
 
+  const gridProps = {
+    ...productsResponse,
+    products: currentProducts || [],
+  }
+
   // The data is considered loading if the length of array is more than 1 - since 'All' is always in the array
   return (
     <PageLayout>
@@ -42,8 +59,8 @@ const Index = () => {
         {loading && <span> Loading categories data.... </span>}
         {categoryList.length > 1 && (
           <>
-            <ul className="flex w-full flex-row flex-wrap justify-center">{tabs}</ul>
-            <ProductGrid productsState={productsResponse} />
+            <ul className="flex w-full flex-row flex-wrap justify-center gap-y-4">{tabs}</ul>
+            <ProductGrid productsState={gridProps} />
           </>
         )}
       </main>
