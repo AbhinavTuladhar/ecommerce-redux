75ddd4898d69dd6217c1d6b76ba28d1647ed5cac
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-19 14:27:40 +0545

    redirect to products instead of home in nav back button

--- src/components/misc/NavigateBackButton.tsx
+++ src/components/misc/NavigateBackButton.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 
 const NavigateBackButton: FC<ButtonProps> = ({ children }) => {
   return (
-    <NavLink to="/">
+    <NavLink to="/products">
       <button className="flex items-center gap-x-2 rounded-lg bg-pink-900 px-6 py-4 font-bold text-white duration-500 fluid-text-lg hover:bg-pink-800">
         <FaArrowLeft className="h-6 w-6" />
         <span> {children} </span>
